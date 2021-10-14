9a562246207a717d1bef053a3071749306552d25
Ramiro Baltazar Vazquez Sanchez <ramirovazquez95@gmail.com>
2021-10-14 08:55:07 -0500

    Change test key

--- evaluaciondocentes/evaluaciondocentes/settings.py
+++ evaluaciondocentes/evaluaciondocentes/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x*g(e-ob-$rsrjc&e%aq24c^7iyt4@0(+s+0in24pde600g(0_'
+SECRET_KEY = os.getenv("SECRET_KEY", None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
